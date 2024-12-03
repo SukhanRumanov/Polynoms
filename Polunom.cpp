eeb114bf03b0482215c614bbaf5d503e0677fdd7
SukhanRumanov <158133569+SukhanRumanov@users.noreply.github.com>
2024-12-03 17:08:40 +0500

    Polunom.cpp

--- Polunom.cpp
+++ Polunom.cpp
@@ -1,6 +1,14 @@
 #include "Polynom.hpp"
 
-void Polynomial::trim() {
+template<int Prime>
+int Polynomial<Prime>::mod(int value) const {
+    value %= Prime;
+    if (value < 0) value += Prime;
+    return value;
+}
+
+template<int Prime>
+void Polynomial<Prime>::trim() {
     while (!coefficients.empty() && coefficients.back() == 0) {
         coefficients.pop_back();
     }
@@ -9,36 +17,48 @@ void Polynomial::trim() {
     }
 }
 
-Polynomial Polynomial::operator+(const Polynomial& other) const {
+
+template<int Prime>
+template<typename T>
+Polynomial<Prime>::Polynomial(T&& coeffs) {
+    static_assert(Prime > 1, "Error");
+
+    coefficients.assign(std::begin(coeffs), std::end(coeffs));
+
+    for (auto& coeff : coefficients) {
+        coeff = mod(coeff);
+    }
+
+    trim();
+
+}
+
+template<int Prime>
+Polynomial<Prime> Polynomial<Prime>::operator+(const Polynomial& other) const {
     size_t maxSize = std::max(coefficients.size(), other.coefficients.size());
     std::vector<int> result(maxSize);
 
     for (size_t i = 0; i < maxSize; ++i) {
         int a = (i < coefficients.size()) ? coefficients[i] : 0;
         int b = (i < other.coefficients.size()) ? other.coefficients[i] : 0;
-        result[i] = a + b;
+        result[i] = mod(a + b);
     }
 
-    return Polynomial(result);
+    return Polynomial(std::move(result));
 }
 
-Polynomial Polynomial::operator*(const Polynomial& other) const {
+template<int Prime>
+Polynomial<Prime> Polynomial<Prime>::operator*(const Polynomial& other) const {
     size_t newSize = coefficients.size() + other.coefficients.size() - 1;
     std::vector<int> result(newSize, 0);
 
     for (size_t i = 0; i < coefficients.size(); ++i) {
         for (size_t j = 0; j < other.coefficients.size(); ++j) {
-            result[i + j] += coefficients[i] * other.coefficients[j];
+            result[i + j] = mod(result[i + j] + mod(coefficients[i] * other.coefficients[j]));
         }
     }
 
-    return Polynomial(result);
+    return Polynomial(std::move(result));
 }
 
-std::ostream& operator<<(std::ostream& os, const Polynomial& poly) {
-    for (size_t i = 0; i < poly.coefficients.size(); ++i) {
-        if (i > 0) os << " + ";
-        os << poly.coefficients[i] << "x^" << i;
-    }
-    return os;
-}
+template class Polynomial<7>;
