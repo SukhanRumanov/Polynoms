68724f5ad50db315e28f71607cfa6cf8e2bd1a01
SukhanRumanov <158133569+SukhanRumanov@users.noreply.github.com>
2024-12-03 17:07:29 +0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 #include <iostream>
 #include "Polynom.hpp"
 
-int main(){
-    Polynomial poly1 = Polynomial(std::vector<int>{1, 2, 3});
-    Polynomial poly2 = Polynomial(std::vector<int>{6, 5, 4});
-    auto sum = poly1 + poly2;     
+int main() {
+    Polynomial<7> poly1(std::vector<int>{1, 2, 3}); 
+    Polynomial<7> poly2(std::vector<int>{ 6, 5, 4 });               
+
+    auto sum = poly1 + poly2;
     auto product = poly1 * poly2;
-    std::cout << "Poly1: " << poly1 << "\n";    
-    std::cout << "Poly2: " << poly2 << "\n";    
-    std::cout << "Sum: " << sum << "\n";        
+
+    std::cout << "Poly1: " << poly1 << "\n";
+    std::cout << "Poly2: " << poly2 << "\n";
+    std::cout << "Sum: " << sum << "\n";
     std::cout << "Product: " << product << "\n";
 
     return 0;
